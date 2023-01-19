f9c742f11d3fa6c3700267d4de612f8d49273f0a
zephsie <zephiezephs@gmail.com>
2023-01-19 19:05:19 +0300

    elasticsearch + logstash + kibana

--- user/src/main/java/com/zephsie/wellbeing/feign/CustomErrorDecoder.java
+++ user/src/main/java/com/zephsie/wellbeing/feign/CustomErrorDecoder.java
@@ -6,6 +6,7 @@ import com.zephsie.wellbeing.utils.exceptions.ServerException;
 import com.zephsie.wellbeing.utils.responses.entity.SingleErrorResponse;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -21,19 +22,14 @@ public class CustomErrorDecoder implements ErrorDecoder {
     }
 
     @Override
+    @SneakyThrows
     public Exception decode(String methodKey, Response response) {
         Response.Body responseBody = response.body();
         HttpStatus responseStatus = HttpStatus.valueOf(response.status());
 
-        if (responseStatus.is5xxServerError()) {
-            return new ServerException();
-        } else if (responseStatus.is4xxClientError()) {
-            try {
-                SingleErrorResponse errorResponse = objectMapper.readValue(responseBody.asInputStream(), SingleErrorResponse.class);
-                return new CustomException(responseStatus.value(), errorResponse.getMessage());
-            } catch (Exception e) {
-                return new ServerException();
-            }
+        if (responseStatus.is4xxClientError()) {
+            SingleErrorResponse errorResponse = objectMapper.readValue(responseBody.asInputStream(), SingleErrorResponse.class);
+            return new CustomException(responseStatus.value(), errorResponse.getMessage());
         }
 
         return new ServerException();
